bb6e02a592fc5c08c6c40f50b2a714ea6679d8e8
Edu-ward <edwardnelson0099@gmail.com>
2025-04-16 15:48:47 +0800

    Fixed scrollbar & is ready for other features.

--- app/components/ui/facultyAccounts.js
+++ app/components/ui/facultyAccounts.js
@@ -1,246 +1,36 @@
-"use client";
-
 import React, { useContext } from "react";
 import { dashboardContext } from "@/app/providers/dashboardProvider";
 
-const facultyList = [
-  {
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },
-  {
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },{
-    _id: 2,
-    lastName: "Dela Cruz",
-    firstName: "Janine",
-    middleName: "O.",
-    contact: "09159130770",
-    email: "maisd@gmail.com",
-  },
-];
+const sendEmail = async (email, status) => {
+  const response = await fetch("/api/send-email", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, status }),
+  });
+
+  if (response.ok) {
+    alert(`Email sent successfully to ${email}`);
+  } else {
+    alert("Error sending email");
+  }
+};
 
 const FacultyAccount = () => {
   const { show } = useContext(dashboardContext);
+
+  const facultyList = [
+    {
+      _id: 2,
+      lastName: "Dela Cruz",
+      firstName: "Janine",
+      middleName: "O.",
+      contact: "09159130770",
+      email: "edwardnelson0099@gmail.com",
+    },
+    // Add more data as needed
+  ];
 
   return (
     <div
@@ -251,7 +41,7 @@ const FacultyAccount = () => {
       <div className="w-[95%] min-h-[95%] relative overflow-hidden bg-white rounded-lg shadow-lg z-0">
         {/* Header */}
         <div className="w-full h-15 bg-tertiary flex-icenter pl-1 text-white">
-          <h1 className="text-2xl p-2">Faculty Accounts</h1>
+          <h1 className="text-2xl font-semibold">Faculty Accounts</h1>
         </div>
 
         {/* Table */}
@@ -264,13 +54,14 @@ const FacultyAccount = () => {
                 <th className="w-[15%]">First Name</th>
                 <th className="w-[15%]">Middle Name</th>
                 <th className="w-[15%]">Contact</th>
-                <th className="w-[17%]">Email</th>
-                <th className="w-[20%]">Action</th>
+                <th className="w-[20%]">Email</th>
+                <th className="w-[15%]">Action</th>
               </tr>
             </thead>
           </table>
         </div>
 
+        {/* Scrollable Body */}
         <div className="overflow-y-scroll max-h-[60vh] hide-scrollbar">
           <table className="table table-fixed w-full border-collapse border border-gray-300 rounded-b-lg">
             <tbody>
@@ -281,13 +72,19 @@ const FacultyAccount = () => {
                   <td className="w-[15%]">{faculty.firstName}</td>
                   <td className="w-[15%]">{faculty.middleName}</td>
                   <td className="w-[15%]">{faculty.contact}</td>
-                  <td className="w-[17%]">{faculty.email}</td>
-                  <td className="w-[20%]">
+                  <td className="w-[20%]">{faculty.email}</td>
+                  <td className="w-[15%]">
                     <div className="flex gap-2 justify-center">
-                      <button className="w-[80px] border border-[#8b0606] text-info font-medium rounded-[5px] btn-success">
+                      <button
+                        className="w-[80px] border border-[#8b0606] text-info font-medium rounded-[5px] btn-success"
+                        onClick={() => sendEmail(faculty.email, "approved")}
+                      >
                         Approved
                       </button>
-                      <button className="w-[70px] border border-[#8b0606] text-[#ffd700] font-medium rounded-[5px] btn-danger ml-2.5">
+                      <button
+                        className="w-[70px] border border-[#8b0606] text-[#ffd700] font-medium rounded-[5px] btn-danger ml-2.5"
+                        onClick={() => sendEmail(faculty.email, "failed")}
+                      >
                         Failed
                       </button>
                     </div>
