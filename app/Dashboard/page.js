0957ec1154feec7a3060c66c76173a7e8f1707bc
gmzn23 <hementera.gemma@gmail.com>
2025-04-28 14:26:06 +0800

    my first commit

--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -8,6 +8,7 @@ import GradeEntry from '../components/ui/GradeEntry'
 import StudentList from '../components/ui/StudentList'
 import GradeView from '../components/ui/GradeView'
 import EnrollmentForm from '../components/ui/EnrollmentForm'
+import InstructorSchedule from "../components/ui/instructorSchedule"
 
 const page = () => {
   const { userAccess } = useContext(dashboardContext);
@@ -42,6 +43,7 @@ const page = () => {
            userAccess === "instructor" && <>
                 <GradeEntry />
                 <StudentList />
+                <InstructorSchedule />
              </>
         } 
         
