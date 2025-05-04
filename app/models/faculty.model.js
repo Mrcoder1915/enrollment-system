90ef6ee39db1bf3e2ea0506e597049ec58ca15ef
Edu-ward <edwardnelson0099@gmail.com>
2025-05-04 22:52:08 +0800

    small changes

--- app/models/faculty.model.js
+++ app/models/faculty.model.js
@@ -2,45 +2,14 @@ import mongoose from "mongoose";
 
 const facultySchema = new mongoose.Schema({
   instructorID: { type: Number, required: true, unique: true },
+  departmentID: { type: Number },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  middleName: { type: String, required: true,},
-  contactNumber: { type: String, required: true },
+  middleName: { type: String },
+  contactNumber: { type: String, required: true, unique: true },
   emailAddress: { type: String, required: true, unique: true },
 }, {
   collection: "facultyList",
-  timestamps: true,
-  instructorID: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
-  departmentID: {
-    type: String,
-  },
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  middleName: {
-    type: String,
-    required: true,
-  },
-  contactNumber: {
-    type: String,
-    required: true,
-  },
-  emailAddress: {
-    type: String,
-    required: true,
-    unique: true,
-  }
-}, {
-  collection: "facultyAccounts",
   timestamps: true,
 });
 
