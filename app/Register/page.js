80ca0da9fcd85b7a5bd6eea3d2f721c5616eb360
rome-git5 <jerome082599@gmail.com>
2025-05-06 21:40:07 +0900

    fix conflict in merging

--- app/Register/page.js
+++ app/Register/page.js
@@ -30,11 +30,11 @@ const Login = () => {
             
         }
     }
-
+ 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 bg-gradient-primary">
      <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">
-          Registration Portal 
+          Registrar Portal 
         </h1>
       <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md h-[500px]">
         
