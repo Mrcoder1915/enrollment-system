e548657d5420a7910724340fa26c018a5d543b82
Edu-ward <edwardnelson0099@gmail.com>
2025-05-10 23:43:26 +0800

    merged

--- app/models/instructorAccount.model.js
+++ app/models/instructorAccount.model.js
@@ -25,12 +25,6 @@ const instructorAccountSchema = new mongoose.Schema({
   timestamps: true,
 });
 
-<<<<<<< HEAD
 const InstructorAccount = mongoose.models.InstructorAccount || mongoose.model("InstructorAccount", instructorAccountSchema);
 
 export default InstructorAccount;
-=======
-const FacultyAccount = mongoose.models.FacultyAccount || mongoose.model("FacultyAccount", facultyAccountSchema);
-
-export default FacultyAccount;
->>>>>>> 256a9a10abfc9b11b21a8a5b59e02c601c3f29c5
