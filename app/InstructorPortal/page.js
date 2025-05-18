6bf5c9b44fd5dea81e8afc69bdd670a69b09c143
gmzn23 <hementera.gemma@gmail.com>
2025-05-19 01:33:53 +0800

    my commit

--- app/InstructorPortal/page.js
+++
@@ -1,9 +0,0 @@
-import InstructorLogin from "./InstructorLogin"
-
-export default function InstructorPortal() {
-  return (
-    <div className="h-screen w-100% flex items-center justify-center bg-gray-100">
-
-      <InstructorLogin />
-    </div>
-  )};
