e40683a9f683359cdc6ef2e50a593187cf8495a2
cynlere <mabalayrecelyn20@gmail.com>
2025-05-20 19:32:04 +0800

    lyncere

--- app/InstructorProfileVerification/page.js
+++ app/InstructorProfileVerification/page.js
@@ -24,7 +24,7 @@ const router = useRouter()
     setFormData((prev) => ({
       ...prev,
       [name]: type === 'checkbox' ? checked : value,
-      // Automatically clear middleName if noMiddleName is checked
+      
       middleName: name === 'noMiddleName' && checked ? '' : prev.middleName,
     }));
   };
